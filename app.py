ca6c2c2fd40e5e505bb7a00e667e001c5e730ac0
Andreas Alexander Maier <andb0t@pm.me>
2021-06-21 09:34:30 +0200

    Only greet

--- app.py
+++ app.py
@@ -14,34 +14,5 @@ def home():
     return 'Hello Back Belt Course!'
 
 
-@app.route("/random/")
-def random_number():
-    """Return a random number."""
-    number = random.random()
-    return f'Your random number: {number}'
-
-
-@app.route("/random/plus")
-def random_number_plus():
-    """Return a random number + the input."""
-    argument = request.args.get('input')
-    try:
-        number = random.random() + float(argument)
-        return f'Your random number plus {argument}: {number}'
-    except ValueError:
-        return f'Your argument has to be a number! ({argument} is an invalid argument)', 400
-
-
-@app.route("/model")
-def my_easy_model():
-    """Return the output of the model given the input."""
-    argument = request.args.get('input')
-    try:
-        output = model.my_difficult_model(argument)
-    except TypeError as exc:
-        return f'Internal server error: {exc}', 500
-    return f'The output given the argument "{argument}": {output}'
-
-
 if __name__ == "__main__":
     app.run(debug=True)
