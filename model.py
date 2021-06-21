0dfaa9bbc8414f8ba975bd8093826dbec77a0799
Andreas Alexander Maier <andb0t@pm.me>
2021-06-21 09:19:08 +0200

    Remove printout

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 def my_difficult_model(argument):
     """A dummy model, returning the length of the input argument."""
-    print(f"Scoring {argument} ...")
     output = len(argument)
     return output
